5e73a8bbf86291dddfd94cc1673b74b9e7594111
9Foxstails <t-mozer@hotmail.com>
2025-03-03 17:03:03 +0300

    feat: make game+round functions
    need to move round function into game

--- script.js
+++ script.js
@@ -6,7 +6,10 @@
 2. Get human choice
     2.1 disregard cases sensitivity,
 3. Keep score
+    3.1 Global scope
 4. Compare choice in rounds total of 5
+    4.1 main function if you will
+    4.2 ends when one of players reaches score of 5
 5. Write message who win round
 6. Write who won at the end
 
@@ -15,9 +18,52 @@
 
 //test
 //console.log("hello world!");
+/***********************************************************************/
+let humanScore=0;
+let computerScore=0;
+
+function playGame(i=0){
+    while (i<5){
+        console.log(playRound());
+        if (i<humanScore){
+            i=humanScore;
+        }
+        else if (i<computerScore){
+            i=computerScore;
+        }
+        else{
+            i=i;
+        }
+    }
+    if (humanScore>computerScore){
+        return "Human wins!";
+    }
+    else {
+        return "Computer wins!";
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice=getHumanChoice();
+    computerChoice=getComputerChoice();
+    
+    if (humanChoice===computerChoice){
+        return "It's a tie!";
+    }
+    else if (humanChoice==="Rock" && computerChoice==="Scissors" ||
+        humanChoice==="Paper" && computerChoice==="Rock" ||
+        humanChoice==="Scissors" && computerChoice==="Paper"){
+            humanScore++;
+            return ("Human wins the round. "+humanChoice+" beats "+computerChoice);
+
+        }
+    else{
+        computerScore++;
+        return ("Computer wins the round. "+computerChoice+" beats "+humanChoice);
+    }
+}
 
 //computer choice thing
-
 function getComputerChoice(randomNumber=0){
     
         while (randomNumber==0){
@@ -40,6 +86,7 @@ function getComputerChoice(randomNumber=0){
             }
 }
 
+//human choice thing
 function getHumanChoice(choice, i=0){
     while(i==0){
         choice=prompt("Rock, Paper, or Scissors?", "");
@@ -65,5 +112,11 @@ function getHumanChoice(choice, i=0){
 
 // }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+
+//testing stuff delete later
+// console.log(getHumanChoice());
+// console.log(getComputerChoice());
+//console.log(playRound());
+console.log(playGame());
+console.log(computerScore);
+console.log(humanScore);
