f0978faf2b0d4dfa46b9d4e8ad3612705a8b4b63
9Foxstails <t-mozer@hotmail.com>
2025-03-03 15:41:07 +0300

    fix: add while loop computer choice

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     1.1 1 to 33 rock, 33 to 66 paper, 67 to 100 scissors.
     1.2 if 0 roll again.
 2. Get human choice
+    2.1 disregard cases sensitivity,
 3. Keep score
 4. Compare choice in rounds total of 5
 5. Write message who win round
@@ -13,29 +14,41 @@
 */
 
 //test
-console.log("hello world!");
+//console.log("hello world!");
 
 //computer choice thing
 
-function getComputerChoice(randomNumber=0, n=0){
+function getComputerChoice(randomNumber=0){
     
-        randomNumber = Math.floor(Math.random()*100);
-        console.log(randomNumber);
+        while (randomNumber==0){
+            randomNumber = Math.floor(Math.random()*100);
+            //console.log(randomNumber);
+        }
+        
         
         if (randomNumber<=33)
             {
-            return "rock";
+            return "Rock";
             }
         else if (randomNumber>33 && randomNumber<=66)
             {
-            return "paper";
+            return "Paper";
             }
         else
             {
-            return "scissors";
+            return "Scissors";
             }
-    
 }
 
-getComputerChoice();
+// function getHumanChoice(choice){
+//     while (choice != 0 )
+//         {
+//         console.log("Human:", choice);
+//         }
+//     console.log("Human:", choice);
+//     return choice;
+// }
+
+//getComputerChoice();
+//console.log(getHumanChoice());
 console.log(getComputerChoice());
