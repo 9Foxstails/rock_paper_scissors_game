53e86e2d7b9b23395d55be76c78887b945365500
9Foxstails <t-mozer@hotmail.com>
2025-03-25 15:06:31 +0300

    feat: add buttons, add basic ui

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
+let rockBtn = document.getElementById('rockBtn');
+let paperBtn = document.getElementById('paperBtn');
+let scissorsBtn = document.getElementById('scissorsBtn');
+let button = document.querySelector('.choice');
 
 let humanScore=0;
 let computerScore=0;
 
-function playGame(i=0){
-    let humanChoice="";
-    let computerChoice="";
+rockBtn.addEventListener('click', playRound);
+paperBtn.addEventListener('click', playRound);
+scissorsBtn.addEventListener('click', playRound);
 
-    for (let i=0;i<5;i++){
-        function playRound(result){
-            humanChoice=getHumanChoice();
-            computerChoice=getComputerChoice();
-            
-            if (humanChoice===computerChoice){
-                result=("It's a tie"
+function playRound(result)
+{
+    humanChoice=getHumanChoice();
+    computerChoice=getComputerChoice();
+        
+    if (humanChoice===computerChoice)
+    {
+        document.getElementById('resultMsg').innerHTML="It's a tie.";
+        result=("It's a tie"
+            +"\nScore: "
+            +"Human "+humanScore
+            +"/ Computer "+computerScore)
+            return console.log(result);
+    }
+    else if (humanChoice==="Rock" && computerChoice==="Scissors" ||
+            humanChoice==="Paper" && computerChoice==="Rock" ||
+            humanChoice==="Scissors" && computerChoice==="Paper")
+            {
+                humanScore++;
+                document.getElementById('resultMsg').innerHTML="Human wins the round!";
+                result=("Human wins the round. "+humanChoice+" beats "+computerChoice
                         +"\nScore: "
                         +"Human "+humanScore
                         +"/ Computer "+computerScore)
                 return console.log(result);
             }
-            else if (humanChoice==="Rock" && computerChoice==="Scissors" ||
-                humanChoice==="Paper" && computerChoice==="Rock" ||
-                humanChoice==="Scissors" && computerChoice==="Paper"){
-                    humanScore++;
-                    result=("Human wins the round. "+humanChoice+" beats "+computerChoice
-                            +"\nScore: "
-                            +"Human "+humanScore
-                            +"/ Computer "+computerScore)
-                    return console.log(result);
-                }
-            else{
-                computerScore++;
-                result=("Computer wins the round. "
-                        +computerChoice
-                        +" beats "
-                        +humanChoice
-                        +"\nScore: "
-                        +"Human "+humanScore
-                        +"/ Computer "+computerScore);
-                return console.log(result);
-            }
-        }
-
-        playRound(humanChoice, computerChoice);
-        
+    else
+    {
+        computerScore++;
+        document.getElementById('resultMsg').innerHTML="Computer wins the round...";
+        result=("Computer wins the round. "
+                +computerChoice
+                +" beats "
+                +humanChoice
+                +"\nScore: "
+                +"Human "+humanScore
+                +"/ Computer "+computerScore);
+        return console.log(result);
     }
+}
+
+function playGame(i=0){
+    let humanChoice="";
+    let computerChoice="";
+    
+    // for (let i=0;i<5;i++){
+    //     playRound(humanChoice, computerChoice);  
+    // }
+
     if (humanScore>computerScore){
         return "Human wins!";
     }
@@ -72,25 +87,21 @@ function getComputerChoice(randomNumber=0){
             }
 }
 
-function getHumanChoice(choice, i=0){
-    while(i==0){
-        choice=prompt("Rock, Paper, or Scissors?", "");
-        choice=choice.slice(0,1).toUpperCase()+choice.slice(1).toLowerCase();
-        if ("Rock".localeCompare(choice)==0){
-            i=1;
-        }
-        else if ("Paper".localeCompare(choice)==0){
-            i=1;
-        }
-        else if("Scissors".localeCompare(choice)==0){
-            i=1;
-        }
-        else{
-            i=0;
-        }
-    }
+function getHumanChoice(choice){
+    choice=event.target.id;
     
-    return choice;
+    if (choice=="rockBtn")
+    {
+        return "Rock";
+    }
+    else if (choice=="paperBtn")
+    {
+        return "Paper";
+    }
+    else
+    {
+        return "Scissors";
+    }
 }
 
 console.log(playGame());
